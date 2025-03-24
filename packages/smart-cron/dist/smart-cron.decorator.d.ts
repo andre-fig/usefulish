0f8722178064dc759703fd9acd4b8ffe65997345
André Figueiredo <afigueiredo@tasken.com>
2025-03-23 21:43:38 -0300

    chore: remove dist

--- packages/smart-cron/dist/smart-cron.decorator.d.ts
+++
@@ -1,7 +0,0 @@
-export interface SmartCronOptions {
-    minutes: 1 | 2 | 3 | 5 | 10 | 15 | 20 | 30 | 60;
-    fromHour?: number;
-    toHour?: number;
-    weekdaysOnly?: boolean;
-}
-export declare function SmartCron(options: SmartCronOptions): <TFunction extends Function, Y>(target: object | TFunction, propertyKey?: string | symbol | undefined, descriptor?: TypedPropertyDescriptor<Y> | undefined) => void;
